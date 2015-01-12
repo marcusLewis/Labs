313241f044934d2a57ea7b753a5b6558253bdd19
lewis.martins@gmail.com <lewis.martins@gmail.com>
2015-01-12 12:39:38 -0200

    ajustes

--- DownloadSolr/src/br/com/rbs/pense/teste/SolrDownload.java
+++ DownloadSolr/src/br/com/rbs/pense/teste/SolrDownload.java
@@ -6,26 +6,31 @@ import java.net.URL;
 
 public class SolrDownload {
 
-    private static final String ANO_MES = "201407";
+    private static final String ANO_MES = "201409";
     
     
     /* PROD */
+    private static final String QUERY = "monthTS%3A" + ANO_MES + "+AND+-origemBusca_parameter%3Aanuncios_similares+AND+-origemBusca_parameter%3Aanuncios_recentes"; // %3A = :
+    
+    //private static final String URL_IMOVEIS = "http://10.240.64.104:8080/solr/pense-imoveis-log_shard1_replica4/select?q=" + QUERY + "&rows=1&wt=xml&indent=true&facet=true&facet.field=q_facet&facet.limit=1000000&facet.mincount=5";
+    private static final String URL_IMOVEIS = "http://localhost:18080/solr/pense-imoveis-log-bkp2/select?q=" + QUERY + "&rows=1&wt=xml&indent=true&facet=true&facet.field=q_facet&facet.limit=1000000&facet.mincount=5";
+    
     //private static final String URL_IMOVEIS = "http://10.240.64.104:8080/solr/pense-imoveis-log_shard1_replica4/select?q=monthTS%3A" + ANO_MES + "&rows=1&wt=xml&indent=true&facet=true&facet.field=q_facet&facet.limit=1000000&facet.mincount=1";
     //private static final String URL_CARROS = "http://10.240.64.104:8080/solr/pense-carros-log_shard-1_replica1/select?q=monthTS%3A" + ANO_MES + "&rows=1&wt=xml&indent=true&facet=true&facet.field=q_facet&facet.limit=1000000&facet.mincount=1";
     
     /* LOCAL */
-    private static final String URL_IMOVEIS = "http://localhost:18080/solr/pense-imoveis-log-bkp/select?q=monthTS%3A" + ANO_MES + "&rows=1&wt=xml&indent=true&facet=true&facet.field=q_facet&facet.limit=1000000&facet.mincount=1";
-    private static final String URL_CARROS = "http://localhost:18080/solr/pense-carros-log-bkp/select?q=monthTS%3A" + ANO_MES + "&rows=1&wt=xml&indent=true&facet=true&facet.field=q_facet&facet.limit=1000000&facet.mincount=1";
+    //private static final String URL_IMOVEIS = "http://localhost:18080/solr/pense-imoveis-log-bkp/select?q=monthTS%3A" + ANO_MES + "&rows=1&wt=xml&indent=true&facet=true&facet.field=q_facet&facet.limit=1000000&facet.mincount=1";
+    //private static final String URL_CARROS = "http://localhost:18080/solr/pense-carros-log-bkp/select?q=monthTS%3A" + ANO_MES + "&rows=1&wt=xml&indent=true&facet=true&facet.field=q_facet&facet.limit=1000000&facet.mincount=1";
     
     
     private static final String FILE_IMOVEIS = "consultas_imoveis.xml";
-    private static final String FILE_CARROS = "consultas_carros.xml";
+    //private static final String FILE_CARROS = "consultas_carros.xml";
     
 
     
     
     public static void main(String[] args) throws Exception {
-        processar(URL_CARROS, FILE_CARROS);
+        //processar(URL_CARROS, FILE_CARROS);
                 
         processar(URL_IMOVEIS, FILE_IMOVEIS);
     }
